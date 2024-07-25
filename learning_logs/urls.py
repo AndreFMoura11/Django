b401882f6fc33166ce844329e41662891b665947
AndreFMoura11 <148908427+AndreFMoura11@users.noreply.github.com>
2024-07-25 16:03:42 -0300

    25 07 24

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from . import views
 
 # Depois da Barra
-# Adiministrar os links
+# Administrar os links
 urlpatterns = [
     path('',views.index, name='index'),
     path('topics',views.topics, name='topics'),
-    #path('topic//',views.topic, name='topic'),
+    path('topics/<topic_id>/',views.topic, name='topic'),
+    # Por ter 3 topicos , para nao criar uma rota para cada assunto vamos colocar topic// mais o assunto especifico
+    # Ai coloca o <topic_id>  
 ]
 
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Topic
-
+from .forms import TopicForm
 def index(request):
     """Pagina principal do learning_log"""
     return render(request,'learning_logs/index.html')
@@ -10,3 +10,18 @@ def topics(request):
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request,'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    """Mostra um único assunto e todas as suas entradas."""
+    topic = Topic.objects.get(id= topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic':topic , 'entries': entries }
+    return request(request, 'learning_logs/topic.html' , context)
+
+def new_topic(request): 
+    """ Adiciona um novo assunto. """
+    if request.method != 'POST':
+        # Nenhum dado submetido; cria um formulario em branco
+        form = TopicForm()
+    else:   
+        # Dados de POST  submetidos; processa os dados

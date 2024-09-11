04c4712b0c7c4448b0b5395a7b2c9e997beb86d1
AndreFMoura11 <148908427+AndreFMoura11@users.noreply.github.com>
2024-09-11 11:25:44 -0300

    11 09 24

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -6,8 +6,11 @@ from . import views
 urlpatterns = [
     path('',views.index, name='index'),
     path('topics',views.topics, name='topics'),
-    path('topics/<topic_id>/',views.topic, name='topic'),
+    path('topic/<topic_id>/',views.topic, name='topic'),
+    path('new_topic',views.new_topic,name='new_topic'),# Notas VVVV Abaixo
     # Por ter 3 topicos , para nao criar uma rota para cada assunto vamos colocar topic// mais o assunto especifico
-    # Ai coloca o <topic_id>  
+    # Ai coloca o <topic_id>
+    path('new_entry/<topic_id>', views.new_entry, name='new_entry')  
 ]
 
+# PRECISA ARRUMAR A TELA DE NEW_ENTRY QUANDO CLIK EM ALGU TOPICO ELE NAO ABRI FICA COM ERRO
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm , EntryForm
 from django.http import HttpResponseRedirect 
 from django.urls import reverse
 
@@ -19,7 +19,7 @@ def topic(request, topic_id):
     topic = Topic.objects.get(id= topic_id)
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic':topic , 'entries': entries }
-    return request(request, 'learning_logs/topic.html' , context)
+    return render(request, 'learning_logs/topic.html' , context)
 
 def new_topic(request): 
     """ Adiciona um novo assunto. """
@@ -28,9 +28,28 @@ def new_topic(request):
         form = TopicForm()
     else:   
         # Dados de POST  submetidos; processa os dados
+        form = TopicForm(request.POST)
         if form.is_valid(): 
             form.save()
             return HttpResponseRedirect(reverse('topics'))
         
     context = {'form': form}
     return render(request ,'learning_logs/new_topic.html',context)
+
+def new_entry(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        #  Nenhum dado submetido cria um formulario em branco
+        form = EntryForm()
+    else:
+        # Dados de POST submetidos processa os dados
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_etry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('topic',args=[topic_id]))
+        
+    context = {'topic':topic, 'form' :form} # Topico e Formulario que foi criado
+    return render(request , 'learning_logs/new_entry.html', context)
